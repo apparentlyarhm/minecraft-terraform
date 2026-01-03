eb28d8c26b4ed3456c66b4a62845cb6d808651db
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2026-01-04 00:23:44 +0530

    small refactor to firewall config

--- firewall.tf
+++ firewall.tf
@@ -4,14 +4,10 @@ resource "google_compute_firewall" "minecraft_allow" {
   network       = "default"
   source_ranges = ["1.1.1.1/32"] // Adding a private placeholder here. This will be edited via our cloud run app.
   
+  // primary game traffic
   allow {
     protocol    = "tcp"
-    ports       = ["25565", "30000"]
-  }
-
-  allow {
-     protocol    = "udp"
-     ports       = ["25565"]
+    ports       = ["25565"]
   }
 
   target_tags = ["mc"] // can leave hardcoded me thinks
@@ -20,13 +16,20 @@ resource "google_compute_firewall" "minecraft_allow" {
 // this firewall rule will allow every IP to view server status, should they know our custom port..
 resource "google_compute_firewall" "motd_allow" {
 
-  name          = "motd-main-${random_string.random.result}"
+  name          = "utils-main-${random_string.random.result}"
   network       = "default"
   source_ranges = ["0.0.0.0/0"]
 
+  // rcon works on TCP
+  allow {
+     protocol    = "tcp"
+     ports       = ["30000"]
+  }
+  
+  // query works on UDP
   allow {
      protocol    = "udp"
-     ports       = ["30000"]
+     ports       = ["25565"]
   }
 
   target_tags = ["mc"] // can leave hardcoded me thinks
